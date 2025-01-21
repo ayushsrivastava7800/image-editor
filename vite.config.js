ece08001dc2b4825a9d2c94a256fa8e81e9db020
ayushsrivastava7800 <160736282+ayushsrivastava7800@users.noreply.github.com>
2025-01-21 22:09:41 +0530

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
-// vite.config.js
-import { defineConfig } from 'vite';
 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      // Example: Proxy requests starting with /api to an external API
-      '/api': {
-        target: 'https://api.example.com', // Target API URL
-        changeOrigin: true, // Change the origin of the host header to the target
-        rewrite: (path) => path.replace(/^\/api/, ''), // Optional: Rewrite the path if necessary
-      },
-    },
-  },
-});
+  plugins: [react()],
+})
